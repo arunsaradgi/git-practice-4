97799be7edae339c15d3405fb7b5e14d6f7e43f4
Arun Saradgi <saradgi07@gmail.com>
2022-10-31 11:24:56 +0530

    adding wrong code

---
+++ pali.js
@@ -0,0 +1,13 @@
+let str = "affa";
+
+let rev_str = "";
+
+for (let i = str.length - 1; i >= 0; i--) {
+  rev_str += str[i];
+}
+
+if (str == rev_str) {
+  console.log("jhsghdhfjhasfjh");
+} else {
+  console.log("no");
+};
---
+++ prime.js
@@ -0,0 +1,13 @@
+let count = 0;
+let num = 23;
+
+for (let i = 2; i <= num / 2; i++) {
+  if (num % i == 0) {
+    count++;
+  }
+}
+if (count == 0) {
+  console.log("iauhdfjnsdhfgasjdbnc");
+} else {
+  console.log("not prime");
+}
